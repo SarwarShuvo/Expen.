28909bb52f09b44831ea029c358e61c96fb0adbb
SarwarShuvo <sarwarshuvo10@gmail.com>
2022-12-25 23:56:53 +0600

    Income and expense showing Design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='container mx-auto text-center'>
+        <div className='container mx-auto text-center pt-6 mb-16'>
             <div className='text-xl'>Powered By <span className='font-bold'>Expen<span className='text-[#F178B6]'>.</span></span> <br />
                 <small>Copywrite © 2022</small>
             </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import income from '../Images/income.png'
+import expense from '../Images/expense.png'
+
 
 const home = () => {
     return (
         <div className='container mx-auto'>
 
-            <div className='grid lg:grid-cols-3 gap-4 md:grid-cols-2 mb-12 m-5'>
+            {/* Showing TOTAL Balance, Income $ Expense   */}
+            <div className='grid lg:grid-cols-3 gap-4 md:grid-cols-2 mb-12 m-5 p-8'>
                 <div className='bg-[#56CCF2] rounded-[36px] pt-6 p-5 pr-10'>
                     <h4 className='text-2xl font-extrabold text-[#6D6D6D] text-left'>Balance</h4>
                     <p className='text-white text-4xl font-extrabold text-right mt-[37.5px] mb-10'>৳ <span>1582585</span> </p>
@@ -19,10 +23,40 @@ const home = () => {
                 </div>
             </div>
             {/* Add new Button  */}
-            <div className='flex justify-end m-5'>
+            <div className='flex justify-end m-5 p-8'>
                 <button class="bg-[#56CCF2]  hover:bg-blue-700 text-white py-2 px-4">
                     Add New
                 </button>
+            </div>
+
+            {/* Showing Expense or Income  */}
+            <div className='p-8'>
+                <div className='bg-[#F6F5F5] p-10 rounded-lg'>
+                    <div className='bg-white flex flex-wrap sm:justify-items-center items-center justify-items-center justify-between sm:justify-between p-2 rounded-md'>
+                        {/* logo  */}
+                        <div className=' flex items-center pl-5'>
+                            <img className='w-20 h-20 p-1' src={income} alt="" />
+                            <p className='font-semibold pl-3'>Salary <br />
+                                <span className='font-normal'>22 Dec 2022</span> </p>
+                        </div>
+                        {/* Amount  */}
+                        <div className='font-semibold pr-16'>
+                            <p><span>50000</span> ৳</p>
+                        </div>
+                    </div>
+                    <div className='bg-white flex flex-wrap sm:justify-items-center items-center justify-items-center justify-between sm:justify-between p-2 rounded-md mt-7'>
+                        {/* logo  */}
+                        <div className=' flex items-center pl-5'>
+                            <img className='w-20 h-20 p-1' src={expense} alt="" />
+                            <p className='font-semibold pl-3'>Shopping <br />
+                                <span className='font-normal'>22 Dec 2022</span> </p>
+                        </div>
+                        {/* Amount  */}
+                        <div className='font-semibold pr-16'>
+                            <p><span>5000</span> ৳</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
